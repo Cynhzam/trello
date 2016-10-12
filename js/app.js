d05e21ff2c2ca6541624f9c13f9668d94a82e044
Cynhzam <cyn.herrerazamudio@gmail.com>
2016-10-11 22:28:33 -0500

    subiendo segunda version

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", function(){
 
+    var box = document.getElementById("box");
     var container = document.getElementById("container");
     var span = document.getElementById("span");
     var form = document.getElementById("form");
@@ -9,6 +10,29 @@ window.addEventListener("load", function(){
     span.addEventListener("click", function(e){
         e.preventDefault();
         span.classList.add("none");
-        form.classList.add("block")
+        form.classList.remove("none");
     });
+
+    btnGuardar.addEventListener("click", function(e){
+        e.preventDefault();
+        agregarForm();
+    });
+
+    function agregarForm(){
+
+        var nuevoForm = document.createElement("div");
+        var titulo = document.createElement("span");
+        var link = document.createElement("a");
+        var contenido = input.value;
+
+        titulo.textContent = contenido;
+        link.textContent = "Añadir una tarjeta";
+
+        form.classList.add("none"); 
+        nuevoForm.classList.add("fondo-form");
+
+        container.appendChild(nuevoForm);
+        nuevoForm.appendChild(titulo);
+        nuevoForm.appendChild(link);
+    }
 });
